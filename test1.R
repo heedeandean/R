d63f215a0dfddc6d40e1eac06306d5531555ff1a
heejin um <44691187+heedeandean@users.noreply.github.com>
2019-03-04 01:22:04 +0900

    Update test1.R

--- test1.R
+++ test1.R
@@ -17,6 +17,7 @@ midwest = as.data.frame(ggplot2::midwest)
 
 mpg[order(-mpg$cty, -mpg$hwy),]
 
+
 # 2)
 # mpg 데이터에서 생산연도별 연료 종류에 따른 통합연비를 연도순으로 출력하시오.
 
@@ -33,6 +34,7 @@ year2$year = 2008
 
 mpg_year_fl = rbind(year1, year2)
 mpg_year_fl[order(mpg_year_fl$year),]
+
 
 # 3)
 # midwest 데이터를 data.frame으로 불러온 후, 데이터의 특징을 설명하시오.
@@ -55,17 +57,29 @@ midwest = rename(midwest, asian = popasian)
 
 colnames(midwest)
 
+
 # 5)
 # total, asian 변수를 이용해 '전체 인구 대비 아시아계 인구 백분율' 파생변수(asianpct)를 추가하고, 
 # 히스토그램을 그려, 도시들이 어떻게 분포하는지 설명하시오.
 
+midwest$asianpct = (midwest$asian / midwest$total) * 100
 
+hist(midwest$asianpct)
 
 
 # 6)
 # 아시아계 인구 백분율(asianpct)의 전체 평균을 구하고, 
 # 평균을 초과하면 "lg", 그 외는 "sm"을 부여하는 파생변수(asianrate)를 추가하는 코드를 작성하시오.
 
+am = mean(midwest$asianpct)
+
+midwest$asianrate = ifelse(midwest$asianpct > am, 'lg', 'sm')
+head(midwest)
+
 
 # 7)
 # "lg"와 "sm"에 해당하는 지역이 얼마나 되는지 빈도 막대그래프(qplot)을 그려보시오.
+
+library(ggplot2)
+
+qplot(midwest$asianrate)
