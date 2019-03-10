baf427140b25e688a9ce37998cdff3ad74f8c237
heejin um <44691187+heedeandean@users.noreply.github.com>
2019-03-10 23:29:54 +0900

    Update fac.R

--- data/fac.R
+++ data/fac.R
@@ -1,11 +1,11 @@
 while(TRUE) {
-  x = as.integer(readline(prompt = "Input the number: "))
-  f = 1
+  x <- as.integer(readline(prompt = "Input the number: "))
+  f <- 1
   
   if (x <= 0) break
   else {
     for (i in 1:x)
-      f = f * i
+      f <- f * i
     
     print(paste("The factorial of", x, "is", f))
   } 
