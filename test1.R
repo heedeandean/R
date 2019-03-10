0ead6071ad49762f0447dcce8d42e4f110a17210
heejin um <44691187+heedeandean@users.noreply.github.com>
2019-03-10 23:16:43 +0900

    Update test1.R

--- test1.R
+++ test1.R
@@ -1,7 +1,7 @@
 # 문항 1. ####
 
 library(ggplot2)
-mpg = as.data.frame(ggplot2::mpg)
+mpg <- as.data.frame(ggplot2::mpg)
 
 # [mpg column]
 # manufacturer: 제조사
@@ -22,7 +22,7 @@ mpg[order(-mpg$cty, -mpg$hwy),]
 
 # 2안.
 
-mpg$cthw = mpg$cty + mpg$hwy
+mpg$cthw <- mpg$cty + mpg$hwy
 head(mpg)
 mpg[order(-mpg$cthw),]
 
@@ -30,7 +30,7 @@ mpg[order(-mpg$cthw),]
 # 2)
 # mpg 데이터에서 생산연도별 연료 종류에 따른 통합연비를 연도순으로 출력하시오.
 
-x = aggregate(data=mpg, cthw~(year+fl), mean)
+x <- aggregate(data = mpg, cthw~(year+fl), mean)
 x[order(x$year),]
 
 
@@ -42,13 +42,13 @@ x[order(x$year),]
 # midwest 데이터를 data.frame으로 불러온 후, 
 # 전체인구와 아시아계인구의 데이터의 특징을 설명하시오.
 
-midwest = as.data.frame(ggplot2::midwest)
+midwest <- as.data.frame(ggplot2::midwest)
 class(midwest)
 
 library('psych')
 describe(midwest)
 colnames(midwest)
-aggregate(data=midwest, cbind(poptotal, popdensity, popasian)~state, mean)
+aggregate(data = midwest, cbind(poptotal, popdensity, popasian)~state, mean)
 
 # 데이터의 특징 : 각 지역에 사는 인구의 특징을 나타냈다.
 #                 (특히, 인종, 민족, 성인/어린이, 빈곤률 등 각 지역의 특징을 나타냈다.)
@@ -65,24 +65,24 @@ aggregate(data=midwest, cbind(poptotal, popdensity, popasian)~state, mean)
 install.packages('dplyr')
 library('dplyr')
 
-midwest = rename(midwest, total = poptotal)
-midwest = rename(midwest, asian = popasian)
+midwest <- rename(midwest, total = poptotal)
+midwest <- rename(midwest, asian = popasian)
 
 colnames(midwest)
 
 # 2안.
 
-colnames(midwest)[5] = 'total'
-colnames(midwest)[10] = 'asian'
+colnames(midwest)[5] <- 'total'
+colnames(midwest)[10] <- 'asian'
 
 
 # 3)
 # 전체 아시아계 인구수와, asian 변수를 이용해 '전체 아시아계 인구 대비 아시아계 인구 백분율' 
 # 파생변수(asianpct)를 추가하고, 히스토그램을 그리시오.
 
-ta = sum(midwest$asian)
+ta <- sum(midwest$asian)
 
-midwest$asianpct = (midwest$asian / ta) * 100
+midwest$asianpct <- (midwest$asian / ta) * 100
 
 hist(midwest$asianpct)
 
@@ -97,7 +97,7 @@ plot(midwest$asianpct)
 
 table(midwest$state)
 
-sa = aggregate(data=midwest, asianpct~state, sum)
+sa <- aggregate(data = midwest, asianpct~state, sum)
 sa
 
 sa[order(-sa$asianpct),] # 아시아계 인구비율로 내림차순.(state 기준) 
@@ -114,9 +114,9 @@ sum(sa$asianpct) # 데이터 검증
 # 아시아계 인구 백분율(asianpct)의 전체 평균을 구하고, 
 # 평균을 초과하면 "lg", 그 외는 "sm"을 부여하는 파생변수(asianrate)를 추가하는 코드를 작성하시오.
 
-am = mean(midwest$asianpct)
+am <- mean(midwest$asianpct)
 
-midwest$asianrate = ifelse(midwest$asianpct > am, 'lg', 'sm')
+midwest$asianrate <- ifelse(midwest$asianpct > am, 'lg', 'sm')
 head(midwest)
 
 
