e0c1723099e7e44364599506a3ce16b7d6683762
heedeandean <7942ahj@hanmail.net>
2019-03-13 15:05:58 +0900

    2019-03-13

--- study4.R
+++ study4.R
@@ -1,1 +1,78 @@
-a
+library(ggplot2)
+library(dplyr)
+
+# 1)
+# mpg데이터에서 연도별 배기량에 따른 도시/고속도로 연비를 꺽은선으로 그리시오.
+# (단, cty는 회색, hwy는 파란색, 2008년은 굵은 선으로 표현하시오)
+
+mpg2 <- mpg %>% 
+        group_by(year, displ) %>%
+        summarise(m1 = mean(cty), m2 = mean(hwy))
+mpg2 
+
+ggplot(mpg2, aes(x=displ)) + 
+  geom_line(aes(y=m1, color='1999 cty')) + 
+  geom_line(aes(y=m2, color='1999 hwy')) +
+  scale_colour_manual("", breaks = c("1999 cty", "1999 hwy"),
+                      values = c("gray", "pink")) +
+  xlab("배기량(cc)") +
+  xlim(1, 7) +
+  scale_y_continuous("연비(M/h)", limits = c(5, 50)) +
+  labs(title = '연도별 통합 연비', subtitle = '(굵은선은 2008년)') 
+
+
+# 2)
+# data(성적.csv) 데이터에서 국어 성적이 80점 이상인
+# 학생들의 수를 성비가 보이도록 학급별로 막대그래프를 그리시오.
+
+options(encoding='utf-8')
+data <- read.csv("data/성적.csv")
+
+d2 <- data %>% 
+      filter(국어 >= 80) %>%
+      group_by(반, 성별) %>%
+      summarise(cnt = n())
+
+d2
+
+ggplot(data = d2, aes(반)) +
+  geom_bar(aes(x = 반, fill=성별),
+           width = 0.7) +
+  scale_fill_discrete(name = "성별") +
+  labs(title = '국어 우수 학생', subtitle = "(80점 이상)")
+  
+
+# 3)
+# 국어 성적이 95점 이상인 학생들의
+# 점수별 밀도그래프를 그리시오.
+
+d3 <- data %>%
+      filter(국어 >= 95)
+d3
+      
+ggplot(d3, aes(국어)) +
+  geom_density(aes(fill=factor(반)), alpha=0.5) +
+  labs(title="반별 국어 우수 학생", subtitle = "(국어 성적 A+)", 
+       x = "성적", y = "밀도", fill = "학급")
+
+
+# 4)
+# midwest데이터에서 전체인구와 아시아계 인구의 
+# 관계를 알아보기 위한 그래프를 그리시오.
+# (단, 전체인구는 50만명 이하, 아시아계인구는 1만명 이하만 표시되게)
+
+midwest <- as.data.frame(ggplot2::midwest)
+head(midwest)
+
+m2 <- midwest %>% filter(poptotal <= 500000 & popasian <= 10000)
+
+ggplot(m2, aes(x=area, y=popasian))+
+       geom_point(aes(col=state, size =popasian))+
+       geom_smooth(method="auto",se=F) + 
+       xlim(c(0, 0.1)) +
+       ylim(c(0, 10000))+
+       labs(subtitle = "Area Vs popasian",
+       y="popasian",
+       x="Area",
+       title = "각 Area 별 아시아계 인구",
+       caption = "Source : ggplot2::midwest") 
