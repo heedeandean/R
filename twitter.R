e4c3844d72507613431dcab5f60d2c90218fa287
heejin um <44691187+heedeandean@users.noreply.github.com>
2019-03-25 11:16:49 +0900

    Delete twitter.R

--- twitter.R
+++
@@ -1,7 +0,0 @@
-Sys.setenv(JAVA_HOME="C:/Program Files/Java/jdk1.8.0_191/")
-
-api_key = "TQA3hvxsZr2Fwfrf03slKKNxB"
-api_secret = "Vzlrg6fgOQm1T8SyNRuARwhi5Z5UJP04KhhwLu4KPJ8f0WwmID"
-token = "1905223106-1pUZ0Eqvz9ojDMvwVwcC4Sm8Lqs8OHkQlz3xiRn"
-token_secret = "bE4hyDyB7e6ZyODJV8RTLpj3AcoqPmYk7QtI4m9XmV0YG"
-setup_twitter_oauth(api_key, api_secret, token, token_secret)
