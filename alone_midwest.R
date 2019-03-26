bb32fd50c832669db204dacedba1107c9f54f0d3
heedeandean <7942ahj@hanmail.net>
2019-03-27 00:10:40 +0900

    2019-03-27

--- alone_midwest.R
+++ alone_midwest.R
@@ -12,6 +12,10 @@ head(midwest)
 
 # 2) 미성년 인구 백분율이 가장 높은 상위 5개 county(지역)와 미성년 인구 백분율을 출력하세요.
 
+midwest %>% 
+  select(county, mi) %>% 
+  arrange(desc(mi)) %>% 
+  head(5)
 
 
 # 3) 미성년 비율 등급 변수를 추가하고, 각 등급에 몇 개의 지역이 있는지 알아보세요.
@@ -20,11 +24,16 @@ midwest <- midwest %>%
               mutate(mi_size = ifelse(mi >= 40, "large", 
                                   ifelse(mi >= 30, "middle", "small")))
 
+table(midwest$mi_size)
 
 
 # 4) popasian은 해당 지역의 아시아인 인구를 나타냅니다. 
 #    '전체 인구 대비 아시아인 인구 백분율' 변수를 추가하고,
 #    하위 10개 지역의 state(주), county(지역), 아시아인 인구 백분율을 출력하세요.
 
-
+midwest %>%
+  mutate(tapct = popasian / poptotal * 100) %>% 
+  arrange(tapct) %>% 
+  select(state, county, tapct) %>% 
+  head(10)
   
--- alone_mpg.R
+++ alone_mpg.R
@@ -140,3 +140,76 @@ mpg <- left_join(mpg, fuel, by = "fl")
 mpg %>% 
   select(model, fl, price_fl) %>% 
   head(5)
+
+
+# -------------------------------------------------------------------------
+# 결측치<NA>.
+
+mpg[c(65, 124, 131, 153, 212), "hwy"] <- NA  # NA 할당.
+
+# 17) drv(구동 방식) 변수와 hwy 변수에 결측치가 몇 개 있는지 알아보세요.
+
+table(is.na(mpg$drv))
+table(is.na(mpg$hwy))
+
+
+# 18) filter()를 이용해 hwy 변수의 결측치를 제외하고, 어떤 drv(구동 방식)의 hwy 평균이 높은지 알아보세요.
+#     (하나의 dplyr 구문으로 만들어야 함.)
+
+mpg %>% 
+  filter(!is.na(hwy)) %>% 
+  group_by(drv) %>% 
+  summarise(mean_hwy = mean(hwy))
+
+# drv 중 f의 hwy 평균이 가장 높습니다.
+
+
+# -------------------------------------------------------------------------
+mpg <- as.data.frame(ggplot2::mpg)
+
+# 이상치 만들기.
+
+mpg[c(10, 14, 58, 93), "drv"] <- "k"
+mpg[c(29, 43, 129, 203), "cty"] <- c(3, 4, 39, 42)
+
+# 19) drv에 이상치가 있는지 확인하세요. 이상치를 결측 처리한 후 이상치가 사라졌는지 확인하세요.
+#     결측 처리를 할 때는 %in% 기호를 활용하세요.
+
+table(mpg$drv) # 이상치 확인.
+
+
+
+mpg %>% filter(is.na(drv))
+
+table(mpg$drv) # 이상치 확인.
+
+
+# 20) 상자 그림을 이용해 cty에 이상치가 있는지 확인하세요.
+#     상자 그림의 통계치를 이용해 정상 범위를 벗어난 값을 결측 처리한 후 
+#     다시 상자 그림을 만들어 이상치가 사라졌는지 확인하세요.
+
+boxplot(mpg$cty)
+boxplot(mpg$cty)$stats
+
+mpg$cty <- ifelse(mpg$cty < 9 | mpg$cty > 26, NA, mpg$cty)
+
+boxplot(mpg$cty)
+
+
+# 21) 이상치를 제외한 다음 drv별로 cty 평균이 어떻게 다른지 알아보세요.
+#     (하나의 dplyr 구문으로 만들어야 함.)
+
+mpg %>% 
+  filter(!is.na(drv) & !is.na(cty)) %>% 
+  group_by(drv) %>% 
+  summarise(mean_cty = mean(cty))
+
+
+
+
+
+
+
+
+
+
