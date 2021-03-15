11a50d304376402b4bba737dbac161c559e72b47
heedeandean <7942ahj@hanmail.net>
2021-03-15 20:26:20 +0900

    R 단축키, 변수명 규칙

---
+++ manual.R
@@ -0,0 +1,15 @@
+# 단축키
+# ALT + - : <-
+# ALT + F4 : 창닫기
+# CTRL + SHIFT + M : %>%
+# CTRL + SHIFT + N : 새 스크립트
+# CTRL + SHIFT + C : 주석처리(블록영역)
+# CTRL + RUN : RUN
+# CTRL + 1 : script 창
+# CTRL + 2 : console 창 
+# CTRL + L : console 창 지우기
+# CTRL + Enter : 한 줄 실행 (블록지정만큼 실행)
+
+# 변수명
+# 1. 문자로 시작
+# 2. 영문(소문자)
