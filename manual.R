451eb2aca318182870e0771a39dd407fef9f5ce6
heedeandean <7942ahj@hanmail.net>
2021-04-30 12:23:34 +0900

    변수& 데이터 구조 정리

--- manual.R
+++ manual.R
@@ -27,6 +27,17 @@
 # R Markdown
 # Ctrl + Alt + i : code chunk
 
-# 개념
+
 # geom_col() : 요약표 기반
 # geom_bar() : 원자료 기반(빈도 막대 그래프 : y값 없음) 
+
+# 변수
+# 1. 연속 변수(Numeric) 
+# 2. 범주 변수(Factor) : levels() 
+
+# 데이터 구조
+# 1. Vector : 1차원 / 한가지 타입
+# 2. Data Frame : 2차원(행,열) / 다양한 타입
+# 3. Matrix : 2차원(행,열) / 한가지 타입
+# 4. Array : N차원의 Matrix
+# 5. List : N차원 / 다양한 데이터 구조 OK
