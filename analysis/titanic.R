ca847128d8d886c5111d6b2a2cb34f4c77d0779c
heedeandean <7942ahj@hanmail.net>
2021-06-21 14:53:24 +0900

    랜덤포레스트 모델 추가

--- analysis/titanic.R
+++ analysis/titanic.R
@@ -1,6 +1,6 @@
 # To Do!
-# 타이타닉 데이터를 이용하여 
-# [선형 회귀/로지스틱 회귀/랜덤포레스트] 중 좋은 모델을 선별해
+# 타이타닉 데이터로
+# [선형 회귀/로지스틱 회귀/랜덤포레스트]모델을 이용하여 
 # 목적변수(Survived:생존)를 예측
 
 rm(list=ls()) # 메모리 삭제
@@ -55,7 +55,7 @@ data %>% map_int(~sum(is.na(.x))) # NA값 처리 확인
 train <- data %>% filter(is_train == TRUE) 
 test <- data %>% filter(is_train == FALSE)
 
-# train -> (train/validation) 
+# train => (train/validation) 
 
 # install.packages('caret')
 library(caret)
@@ -82,7 +82,7 @@ glimpse(train)
 
 # model1. 선형 회귀
 lm.model <- lm(Survived~PassengerId+Pclass+Sex+Age+SibSp+Parch+Fare+Embarked,
-               family = 'binomial', data = data.train)
+               data = data.train)
 summary(lm.model)
 
 model1 <- step(lm.model, direction = 'both') 
@@ -101,21 +101,18 @@ summary(logit.model)
 model2 <- step(logit.model, direction = 'both') 
 summary(model2)
 
+
 # model3. 랜덤포레스트
 # install.packages('randomForest')
 library(randomForest)
 
 set.seed(42)
 rf <- randomForest(Survived~PassengerId+Pclass+Sex+Age+SibSp+Parch+Fare+Embarked,
-                   data = train, ntree=200, importance=T)
-summary(rf)
+                   data = data.train, ntree=200, importance=T)
+
 varImpPlot(rf, type=1, pch=19, col=1, cex=1, main='')
-
-predictions <- predict(rf, newdata = data.result)
-
-
 #====================================================
-# 6. validation : validation 데이터
+# 6. validation
 data.valid$Survived # 정답
 
 pred1 <- predict(model1, newdata = data.valid, type = 'response')
@@ -124,19 +121,28 @@ pred1
 pred2 <- predict(model2, newdata = data.valid, type = 'response')
 pred2
 
+pred3 <- predict(rf, newdata = data.valid)
+pred3
+
 # install.packages('e1071')
 # 혼동행렬(예측값, 정답)
 cm1 <- confusionMatrix(factor(if_else(pred1 > 0.5, 1, 0)), factor(data.valid$Survived))
-cm1 # Accuracy : 0.8165 (cf. No Information Rate : 찍음)
+cm1 # No Information Rate : 찍음
 
 cm2 <- confusionMatrix(factor(if_else(pred2 > 0.5, 1, 0)), factor(data.valid$Survived)) 
-cm2 # Accuracy : 0.8015
+cm2 
 
-ls(cm2)
-cm2$byClass # F1 높을수록 좋다. 
+cm3 <- confusionMatrix(factor(if_else(pred3 > 0.5, 1, 0)), factor(data.valid$Survived))
+cm3  
 
+ls(cm1)
+
+# F1 높을수록 좋다. 
+cm1$byClass[7] 
+cm2$byClass[7]
+cm3$byClass[7]
 #====================================================
-# 7. output : Accuracy가 더 높은 선형회귀모델 선택
+# 7. output : test 데이터
 
 sub <- read.csv('gender_submission.csv') # 정답
 sub
@@ -145,8 +151,9 @@ data.result <- inner_join(test, sub, by='PassengerId')
 data.result
 
 data.result.pred <- predict(model1, newdata = data.result, type='response')
-data.result.pred
+# data.result.pred <- predict(model2, newdata = data.result, type='response')
+# data.result.pred <- predict(rf, newdata = data.result)
 
 confusionMatrix(factor(if_else(data.result.pred > 0.5, 1, 0)), 
-                factor(data.result$Survived.y)) # Accuracy : 0.9713  
+                factor(data.result$Survived.y))   
 #====================================================
